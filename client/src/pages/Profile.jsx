2978aacea717e9ca2e69636664e1775d1b07f06f
Johnakins <johnolawole1@gmail.com>
2023-12-10 23:12:26 +0100

    add sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react"
 import { useSelector } from "react-redux"
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
 import {app} from "../firebase"
-import {updateUserStart, updateUserSuccess, updateUserFailure,deleteUserStart, deleteUserSuccess, deleteUserFailure} from "../redux/user/userSlice.js"
+import {updateUserStart, updateUserSuccess, updateUserFailure,deleteUserStart, deleteUserSuccess, deleteUserFailure, signOutUserStart, signOutUserSuccess, signOutUserFailure} from "../redux/user/userSlice.js"
 import { useDispatch } from "react-redux"
 
 const Profile = () => {
@@ -88,6 +88,21 @@ const Profile = () => {
           dispatch(deleteUserFailure(error.message))
         }
       }
+
+      const handleSignOut = async () => {
+        try {
+          dispatch(signOutUserStart());
+          const res = await fetch('/api/auth/signout')
+          const data = await res.json();
+          if (data.success === false) {
+            dispatch(signOutUserFailure(data.message));
+            return;
+          }
+          dispatch(signOutUserSuccess(data)); 
+        } catch (error) {
+          dispatch(signOutUserFailure(error.message))
+        }
+      }
     
     return (
         <div className="p-3 max-w-lg mx-auto">
@@ -108,7 +123,7 @@ const Profile = () => {
                 <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">{loading ? "loading..." : "Update"}</button>
                 <div className="flex justify-between mt-5">
                     <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
-                    <span className="text-red-700 cursor-pointer">Sign out</span>
+                    <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
                 </div>
                 <p className="text-green-700 mt-5">{updateSuccess ? "User is updated successfully" : ""}</p>
             </form>
